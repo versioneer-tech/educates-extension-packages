e398c42d3a27c18170b70451c6eaa65cb2855bbf
Jorge Morales Pou <jorgemoralespou@gmail.com>
2024-04-12 13:42:15 +0200

    Fixing permissions

--- packages/argocd/v2.10.6/setup.d/01-setup-binary.sh
+++ packages/argocd/v2.10.6/setup.d/01-setup-binary.sh
@@ -5,4 +5,8 @@ set -eo pipefail
 
 PACKAGE_DIR=$(cd "$(dirname $BASH_SOURCE)/.."; pwd)
 
+# Rename the binary
 mv $PACKAGE_DIR/bin/argocd-linux-amd64 $PACKAGE_DIR/bin/argocd 
+
+# Make the binary executable
+chmod 755 $PACKAGE_DIR/bin/argocd
--- packages/crane/v0.19.1/setup.d/01-setup-binary.sh
+++ packages/crane/v0.19.1/setup.d/01-setup-binary.sh
@@ -5,4 +5,5 @@ set -eo pipefail
 
 PACKAGE_DIR=$(cd "$(dirname $BASH_SOURCE)/.."; pwd)
 
+# Make the binary executable
 chmod 755 $PACKAGE_DIR/bin/crane
