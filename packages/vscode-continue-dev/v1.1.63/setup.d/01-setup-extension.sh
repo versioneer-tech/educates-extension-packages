a0df8ac7f513271a973e223b42140c147f5b1a77
Jorge Morales Pou <jorgemoralespou@gmail.com>
2025-07-28 16:46:52 +0200

    Fixing continue.dev extension for x64

--- packages/vscode-continue-dev/v1.1.63/setup.d/01-setup-extension.sh
+++ packages/vscode-continue-dev/v1.1.63/setup.d/01-setup-extension.sh
@@ -6,6 +6,12 @@ set -eo pipefail
 PACKAGE_DIR=$(cd "$(dirname $BASH_SOURCE)/.."; pwd)
 
 # Install extensions
-code-server --install-extension $PACKAGE_DIR/extensions/$PLATFORM_ARCH/Continue.continue-1.1.63@linux-$PLATFORM_ARCH.vsix
+# When platform arch is amd64, we need to modify the variable used to x64
+PLATFORM=$PLATFORM_ARCH
+if [ "$PLATFORM_ARCH" = "amd64" ]; then
+  PLATFORM="x64"
+fi
+
+code-server --install-extension $PACKAGE_DIR/extensions/$PLATFORM/Continue.continue-1.1.63@linux-$PLATFORM.vsix
 
 unset PACKAGE_DIR
